2f293c3b4b6ac0ea9c6339ecc1e791f9cf4dd2df
Jonathan <many.d.r.o.2001@hotmail.com>
2024-03-12 22:24:48 -0500

    Clase de Productos creada con sus atributos y métodos

---
+++ main/java/com/mycompany/inventario_tienda_compensar/Product.java
@@ -0,0 +1,85 @@
+package com.mycompany.inventario_tienda_compensar;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Product {
+    private String name;
+    private String type;
+    private int totalUnits;
+    private double unitaryValue;
+    private double iva;
+    private double totalValue;
+
+    public Product(String name, String type, int totalUnits, double unitaryValue) {
+        this.name = name;
+        this.type = type;
+        this.totalUnits = totalUnits;
+        this.unitaryValue = unitaryValue;
+        this.iva = calculateIva();
+        this.totalValue = calculateTotalValue();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getTotalUnits() {
+        return totalUnits;
+    }
+
+    public void setTotalUnits(int totalUnits) {
+        this.totalUnits = totalUnits;
+    }
+
+    public double getUnitaryValue() {
+        return unitaryValue;
+    }
+
+    public void setUnitaryValue(double unitaryValue) {
+        this.unitaryValue = unitaryValue;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+    public double getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(double totalValue) {
+        this.totalValue = totalValue;
+    }
+
+    private double calculateIva(){
+        final Map<String, Double> IVA_VALUES = new HashMap<>();
+        IVA_VALUES.put("Aseo", .19);
+        IVA_VALUES.put("Papelería", .09);
+        IVA_VALUES.put("Víveres", .15);
+        IVA_VALUES.put("Mascotas", .16);
+        IVA_VALUES.put("Otros", .1);
+        return IVA_VALUES.get(type);
+    }
+
+    private double calculateTotalValue(){
+        return unitaryValue * totalUnits;
+    }
+
+}
