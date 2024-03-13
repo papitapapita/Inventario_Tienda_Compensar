4f63608f62cddafd0fd185bb9dfcc360519bab3a
Jonathan <many.d.r.o.2001@hotmail.com>
2024-03-12 20:54:16 -0500

    Files created

---
+++ main/java/com/mycompany/inventario_tienda_compensar/Inventario_Tienda_Compensar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventario_tienda_compensar;
+
+/**
+ *
+ * @author manyd
+ */
+public class Inventario_Tienda_Compensar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
